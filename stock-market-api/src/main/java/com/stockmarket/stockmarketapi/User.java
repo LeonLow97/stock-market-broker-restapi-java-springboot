a2dedf7216daae0cf534b61446e663ba78a3be76
LeonLow97 <lowjiewei@gmail.com>
2023-02-03 10:19:21 +0800

    Created user handlers for login, logout and signup

---
+++ stock-market-api/src/main/java/com/stockmarket/stockmarketapi/User.java
@@ -0,0 +1,48 @@
+package com.stockmarket.stockmarketapi;
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String isAdmin;
+
+    public User(String username, String password, String email, String isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIsAdmin() {
+        return this.isAdmin;
+    }
+
+    public void setIsAdmin(String isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+}
