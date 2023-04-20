1c7dd0bdccb6a74c43696c1e63319b4cb7ff3b0a
LeonLow97 <lowjiewei@gmail.com>
2023-04-21 00:21:56 +0800

    REST API Swagger documentation on Portfolio Controller

--- stock-market-api/src/main/java/com/stockmarket/stockmarketapi/controllers/PortfolioController.java
+++ stock-market-api/src/main/java/com/stockmarket/stockmarketapi/controllers/PortfolioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.stockmarket.stockmarketapi.entity.Portfolio;
 import com.stockmarket.stockmarketapi.service.PortfolioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Portfolio Controller",
+    description = "To retrieve the portfolio of the user to view stocks that have been purchased.")
 @RestController
 @RequestMapping("/api")
 public class PortfolioController {
@@ -19,14 +29,32 @@ public class PortfolioController {
   @Autowired
   PortfolioService portfolioService;
 
-  @GetMapping("/portfolio")
+  @Operation(summary = "Get Portfolio",
+      description = "Retrieves the entire portfolio of stocks of the user.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200",
+          description = "Successfully retrieved the portfolio of the user.",
+          content = @Content(
+              array = @ArraySchema(schema = @Schema(implementation = Portfolio.class)))),
+      @ApiResponse(responseCode = "404", description = "NOT FOUND - portfolio does not exist",
+          content = @Content)})
+  @GetMapping(value = "/portfolio", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<List<Portfolio>> getPortfolio(HttpServletRequest request) {
     Integer userId = (Integer) request.getAttribute("userId");
     List<Portfolio> portfolio = portfolioService.getPortfolio(userId);
     return new ResponseEntity<>(portfolio, HttpStatus.OK);
   }
 
-  @GetMapping("/portfolio/{portfolioId}")
+  @Operation(summary = "Get Portfolio Stock",
+      description = "Retrieves the details of a particular stock in the portfolio of the user.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200",
+          description = "Successfully retrieved the details of the stock of the user.",
+          content = @Content(schema = @Schema(implementation = Portfolio.class))),
+      @ApiResponse(responseCode = "404",
+          description = "NOT FOUND - stock does not exist in user's portfolio.",
+          content = @Content)})
+  @GetMapping(value = "/portfolio/{portfolioId}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Portfolio> getPortfolioStock(HttpServletRequest request,
       @PathVariable("portfolioId") Integer portfolioId) {
     Integer userId = (Integer) request.getAttribute("userId");
