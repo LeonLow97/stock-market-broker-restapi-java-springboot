bff8b07c152f099f313cecefb396d482c7924a26
LeonLow97 <lowjiewei@gmail.com>
2023-04-21 19:20:59 +0800

    Added Schema Swagger documentation to UserRegisterDTO

--- stock-market-api/src/main/java/com/stockmarket/stockmarketapi/DTOs/UserRegisterDTO.java
+++ stock-market-api/src/main/java/com/stockmarket/stockmarketapi/DTOs/UserRegisterDTO.java
@@ -2,6 +2,7 @@ package com.stockmarket.stockmarketapi.DTOs;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,8 +12,17 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserRegisterDTO {
 
+    @Schema(description = "Username for the newly registered user for an account.",
+            example = "leonlow", required = true, name = "username", type = "String")
     public String username;
+
+    @Schema(description = "Email for the newly registered user for an account.",
+            example = "leonlow@email.com", format = "email", required = true, name = "email",
+            type = "String")
     public String email;
+
+    @Schema(description = "Password for the newly registered user for an account.",
+            example = "Password0!", required = true, name = "password")
     @JsonProperty(access = Access.WRITE_ONLY)
     public String password;
 
