de3f34aca85d65450d151a8c7a74dc6162a5b31c
LeonLow97 <lowjiewei@gmail.com>
2023-04-21 20:31:43 +0800

    Added Schema Swagger documentation to OrderDTO

--- stock-market-api/src/main/java/com/stockmarket/stockmarketapi/DTOs/OrderSubmitDTO.java
+++ stock-market-api/src/main/java/com/stockmarket/stockmarketapi/DTOs/OrderSubmitDTO.java
@@ -1,5 +1,6 @@
 package com.stockmarket.stockmarketapi.DTOs;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,11 +8,23 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@Schema(description = "Data Transfer Object for submitting the necessary fields needed to create an order by the user.")
 public class OrderSubmitDTO {
-    
+
+    @Schema(description = "Stock ticker of the stock the user wants to buy/sell.", example = "AAPL",
+            required = true)
     private String stockTicker;
+
+    @Schema(description = "Order type refers to the action which can only be 'BUY' or 'SELL'",
+            example = "BUY", allowableValues = {"BUY", "SELL"}, required = true)
     private String orderType;
+
+    @Schema(description = "No of shares the user wants to BUY/SELL in the order.", example = "60",
+            required = true)
     private int noOfShares;
+
+    @Schema(description = "Price of the stock the user is willing to BUY/SELL in the order.",
+            example = "149.86", required = true)
     private double cost;
 
 }
--- stock-market-api/src/main/java/com/stockmarket/stockmarketapi/entity/Order.java
+++ stock-market-api/src/main/java/com/stockmarket/stockmarketapi/entity/Order.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -21,8 +22,13 @@ import lombok.Setter;
 @Setter
 @RequiredArgsConstructor
 @NoArgsConstructor
+@Schema(name = "OrderDTO",
+    description = "Data Transfer Object of the orders associated with the user, returns the history of all the orders.")
 public class Order {
 
+  @Schema(
+      description = "Unique identifier of the user which is a foreign key to the 'accounts' table.",
+      example = "1", required = true)
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "order_id")
@@ -33,22 +39,30 @@ public class Order {
   @Column(name = "user_id", nullable = false)
   private long userId;
 
+  @Schema(description = "Stock ticker of the stock associated with the order.", example = "AAPL",
+      required = true)
   @NonNull
   @Column(name = "stock_ticker", nullable = false)
   private String stockTicker;
 
+  @Schema(description = "Order type of the stock associated with the order.", example = "BUY",
+      required = true)
   @NonNull
   @Column(name = "order_type", nullable = false)
   private String orderType;
 
+  @Schema(description = "Number of shares transacted in the order for the stock.", example = "60",
+      required = true)
   @NonNull
   @Column(name = "no_of_shares", nullable = false)
   private int noOfShares;
 
+  @Schema(description = "Price purchased/sold for each order.", example = "143.21", required = true)
   @NonNull
   @Column(name = "cost", nullable = false)
   private double cost;
 
+  @Schema(description = "Order date for each order.", example = "2023-04-22 12:10:33", required = true)
   @Column(name = "order_date", nullable = false)
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime orderDate = LocalDateTime.now();
